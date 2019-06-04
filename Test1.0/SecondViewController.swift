029486d1edf1f2c447f7d38f76b18940ec2ef7c0
ShrutikaSingh <shrutika051220@gmail.com>
2019-06-04 10:59:39 +0530

    Initial Commit

---
+++ Test1.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Test1.0
+//
+//  Created by Shrutika on 04/06/19.
+//  Copyright © 2019 Shrutika. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
